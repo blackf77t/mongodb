9ae56aa386e992c23f026df53bd53e60fab9f828
tmonte007 <tmonte@gmail.com>
2017-10-01 17:20:59 -0400

    Create dbinsert.py

---
+++ dbinsert.py
@@ -0,0 +1,6 @@
+import testdblib
+
+inserter = testdblib.db_insert
+
+# Data entry
+inserter("Tom Monte", "Verizon Wireless", "Devops Engineer", "@tmontetweets", ["account management", "ansible", "python"])
